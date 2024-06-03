a600a197e2b48cfa9b46af879cdd3fd78e045b75
Natali Udovyk <natali.udovik@gmail.com>
2024-06-03 20:39:54 +0300

    correct-3

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,22 +3,6 @@ const formData = {
     message: "" 
 }
 
-// const fieldMessage = document.querySelector('.feedback-form')
-// fieldMessage.addEventListener('submit', event => {
-//     event.preventDefault();
-//     const form = event.target;
-//     const email = form.elements.email.value.trim();
-//     const message = form.elements.message.value.trim();
-//     if(email === '' || message === ''){
-//         return alert('Fill please all fields')
-//     } else {
-//         userForm.email = email,
-//         userForm.message = message
-//         console.log(formData);
-//         form.reset();
-//     }
-    
-// })
 const form = document.querySelector(".feedback-form");
 const localStorageKey = "feedback-form-state";
 
@@ -29,12 +13,11 @@ form.addEventListener("input", (evt) => {
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
   const email = form.elements.email.value.trim();
-    const message = form.elements.message.value.trim();
-	
+  const message = form.elements.message.value.trim();
+  if(email === '' || message === ''){
+    return alert('Fill please all fields')
+}
   localStorage.removeItem(localStorageKey);
   form.reset();
-  if(email === '' || message === ''){
-            return alert('Fill please all fields')
-        }
 });
 console.log(formData);
