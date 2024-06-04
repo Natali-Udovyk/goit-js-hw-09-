4a47c7365a6a5c78f4a80245256f8e4a38df4158
Natali Udovyk <natali.udovik@gmail.com>
2024-06-05 01:13:11 +0300

    all done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,42 +3,29 @@ const formData = {
     message: "" 
 }
 const form = document.querySelector(".feedback-form");
-const textarea = form.elements.message;
-const input = form.elements.email;
 const localStorageKey = "feedback-form-state";
-
-textarea.value = localStorage.getItem(localStorageKey) ?? "";
+const savedData = localStorage.getItem(localStorageKey);
+form.elements.message.value = localStorage.getItem(localStorageKey) ?? "";
 
 form.addEventListener("input", (evt) => {
-  localStorage.setItem(localStorageKey, evt.target.value);
+    formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
-	console.log(evt.target.elements.message.value);
+  const email = form.elements.email.value.trim();
+  const message = form.elements.message.value.trim();
+  if(email === '' || message === ''){
+    return alert('Fill please all fields')
+}
+  console.log(formData)
   localStorage.removeItem(localStorageKey);
   form.reset();
+  formData.email = '';
+  formData.message = '';
 });
 
-// const form = document.querySelector(".feedback-form");
-// const localStorageKey = "feedback-form-state";
-// form.elements.message.value = localStorage.getItem(localStorageKey) ?? "";
-//   form.addEventListener("input", (evt) => {
-//     localStorage.setItem(localStorageKey, evt.target.value);
-//   });
-
-// form.addEventListener("submit", (evt) => {
-//   evt.preventDefault();
-//   const email = form.elements.email.value.trim();
-//   const message = form.elements.message.value.trim();
-//   if(email === '' || message === ''){
-//     return alert('Fill please all fields')
-// }
-// console.log(formData);
-// localStorage.removeItem(localStorageKey);
-// form.reset();
-
-// });
 
 
-// localStorage.setItem(localStorageKey, JSON.stringify(formData));
+
