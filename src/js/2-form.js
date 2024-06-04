eb8ff8a49707113494be2abfbf510b074291312c
Natali Udovyk <natali.udovik@gmail.com>
2024-06-04 22:30:01 +0300

    correct-4

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,6 +6,16 @@ const formData = {
 const form = document.querySelector(".feedback-form");
 const localStorageKey = "feedback-form-state";
 
+window.addEventListener("load", () => {
+    const savedData = localStorage.getItem(localStorageKey);
+    if (savedData) {
+      const savedFormData = JSON.parse(savedData);
+      form.elements.email.value = savedFormData.email || '';
+      form.elements.message.value = savedFormData.message || '';
+      formData.email = savedFormData.email || '';
+      formData.message = savedFormData.message || '';
+    }
+  });
 form.addEventListener("input", (evt) => {
   localStorage.setItem(localStorageKey, evt.target.value);
 });
@@ -17,8 +27,10 @@ form.addEventListener("submit", (evt) => {
   if(email === '' || message === ''){
     return alert('Fill please all fields')
 }
-  localStorage.removeItem(localStorageKey);
-  form.reset();
-});
-localStorage.setItem("formData", JSON.stringify(formData));
 console.log(formData);
+localStorage.removeItem(localStorageKey);
+form.reset();
+formData.email = '';
+formData.message = '';
+});
+
