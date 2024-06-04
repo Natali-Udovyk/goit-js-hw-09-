9dfbd55d97240456182ff9248894013d8ec5d496
Natali Udovyk <natali.udovik@gmail.com>
2024-06-04 23:49:59 +0300

    Correct-4

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,35 +2,43 @@ const formData = {
     email: "",
     message: "" 
 }
-
 const form = document.querySelector(".feedback-form");
+const textarea = form.elements.message;
+const input = form.elements.email;
 const localStorageKey = "feedback-form-state";
 
-window.addEventListener("load", () => {
-    const savedData = localStorage.getItem(localStorageKey);
-    if (savedData) {
-      const savedFormData = JSON.parse(savedData);
-      form.elements.email.value = savedFormData.email || '';
-      form.elements.message.value = savedFormData.message || '';
-      formData.email = savedFormData.email || '';
-      formData.message = savedFormData.message || '';
-    }
-  });
+textarea.value = localStorage.getItem(localStorageKey) ?? "";
+
 form.addEventListener("input", (evt) => {
   localStorage.setItem(localStorageKey, evt.target.value);
 });
 
 form.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  const email = form.elements.email.value.trim();
-  const message = form.elements.message.value.trim();
-  if(email === '' || message === ''){
-    return alert('Fill please all fields')
-}
-console.log(formData);
-localStorage.removeItem(localStorageKey);
-form.reset();
-formData.email = '';
-formData.message = '';
+	console.log(evt.target.elements.message.value);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
 });
 
+// const form = document.querySelector(".feedback-form");
+// const localStorageKey = "feedback-form-state";
+// form.elements.message.value = localStorage.getItem(localStorageKey) ?? "";
+//   form.addEventListener("input", (evt) => {
+//     localStorage.setItem(localStorageKey, evt.target.value);
+//   });
+
+// form.addEventListener("submit", (evt) => {
+//   evt.preventDefault();
+//   const email = form.elements.email.value.trim();
+//   const message = form.elements.message.value.trim();
+//   if(email === '' || message === ''){
+//     return alert('Fill please all fields')
+// }
+// console.log(formData);
+// localStorage.removeItem(localStorageKey);
+// form.reset();
+
+// });
+
+
+// localStorage.setItem(localStorageKey, JSON.stringify(formData));
