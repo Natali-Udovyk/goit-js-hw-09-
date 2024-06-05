2c81d475f2a05eef69ea8ee0f9787e4495a34acd
Natali Udovyk <natali.udovik@gmail.com>
2024-06-05 22:05:43 +0300

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,9 +4,17 @@ const formData = {
 }
 const form = document.querySelector(".feedback-form");
 const localStorageKey = "feedback-form-state";
-const savedData = localStorage.getItem(localStorageKey);
-form.elements.message.value = localStorage.getItem(localStorageKey) ?? "";
 
+// form.elements.email.value = JSON.parse(localStorage.getItem(localStorageKey)).email ?? "";
+
+const savedData = localStorage.getItem(localStorageKey);
+if (savedData) {
+    const parsedData = JSON.parse(savedData);
+    formData.email = parsedData.email || "";
+    formData.message = parsedData.message || "";
+    form.elements.email.value = formData.email;
+    form.elements.message.value = formData.message;
+}
 form.addEventListener("input", (evt) => {
     formData[evt.target.name] = evt.target.value;
   localStorage.setItem(localStorageKey, JSON.stringify(formData));
